6818705d73bf4b4dfd05248b9332209b14935a5b
Crimcat <adriangutcarb@gmail.com>
2024-07-23 20:13:40 -0600

    ic

--- SegundoParcial2Q2024/Capa_Vistas/Ingresar_Candidatos.aspx
+++ SegundoParcial2Q2024/Capa_Vistas/Ingresar_Candidatos.aspx
@@ -1,8 +1,30 @@
 ﻿<%@ Page Language="C#" MasterPageFile="Pagina.Master" AutoEventWireup="true" CodeBehind="Ingresar_Candidatos.aspx.cs" Inherits="SegundoParcial2Q2024.Capa_Vistas.Ingresar_Candidatos" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+        #Text1 {
+            width: 213px;
+        }
+    </style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <h1>Ingresar Candidato </h1>
-    <asp:Label ID="lmensaje" runat="server" Text="Label"></asp:Label>
+    <div>
+        <h3>Por favor, ingrese los datos que se le solicitan</h3>
+        <br />
+        <asp:Label ID="Lnombre" runat="server" Text="Nombre:"></asp:Label> 
+        <asp:TextBox ID="TBnombre" runat="server" Width="221px" Height="16px"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Lcedula" runat="server" Text="Cédula:"></asp:Label>
+        <asp:TextBox ID="TBcedula" runat="server" Width="221px" Height="16px"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Lpartido" runat="server" Text="Partido:"></asp:Label>
+        <asp:TextBox ID="TBpartido" runat="server" Width="221px"></asp:TextBox>
+
+
+    </div>
 </asp:Content>
--- SegundoParcial2Q2024/Capa_Vistas/Ingresar_Candidatos.aspx.designer.cs
+++ SegundoParcial2Q2024/Capa_Vistas/Ingresar_Candidatos.aspx.designer.cs
@@ -15,12 +15,57 @@ namespace SegundoParcial2Q2024.Capa_Vistas
     {
 
         /// <summary>
-        /// Control lmensaje.
+        /// Control Lnombre.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lmensaje;
+        protected global::System.Web.UI.WebControls.Label Lnombre;
+
+        /// <summary>
+        /// Control TBnombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBnombre;
+
+        /// <summary>
+        /// Control Lcedula.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Lcedula;
+
+        /// <summary>
+        /// Control TBcedula.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBcedula;
+
+        /// <summary>
+        /// Control Lpartido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Lpartido;
+
+        /// <summary>
+        /// Control TBpartido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBpartido;
     }
 }
